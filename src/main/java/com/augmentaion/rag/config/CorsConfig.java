8736cdaff0d0e535871bc26bb10580a2719a067e
lavish-chhabra <lavishchhabra11@gmail.com>
2026-06-27 12:16:41 +0530

    allowed vercel endpoint as we are deploying front end to vercel

--- src/main/java/com/augmentaion/rag/config/CorsConfig.java
+++ src/main/java/com/augmentaion/rag/config/CorsConfig.java
@@ -17,7 +17,8 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173",
+                                "https://your-vercel-app.vercel.app")
                         .allowedMethods("*");
             }
         };
